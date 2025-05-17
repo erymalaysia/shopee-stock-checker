a08dffdc07446b388ec2d9731e9ab97b2b9941e7
erymalaysia <gnomefan@gmail.com>
2025-05-18 02:51:21 +0800

    Update google.js

--- google.js
+++ google.js
@@ -1,22 +1,40 @@
-const { google } = require("googleapis");
+const { google } = require('googleapis');
 
-async function getSheetData(sheetId, sheetName, auth) {
-  const sheets = google.sheets({ version: "v4", auth });
+// Parse JSON credentials from environment variable
+const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_JSON);
+
+const auth = new google.auth.GoogleAuth({
+  credentials: serviceAccount,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
+
+async function getSheetData(sheetId, range) {
+  const authClient = await auth.getClient();
+  const sheets = google.sheets({ version: 'v4', auth: authClient });
+
   const res = await sheets.spreadsheets.values.get({
     spreadsheetId: sheetId,
-    range: `${sheetName}!A2:A`
+    range: range,
   });
-  return res.data.values.flat();
+
+  return res.data.values;
 }
 
-async function updateSheetData(sheetId, sheetName, values, auth) {
-  const sheets = google.sheets({ version: "v4", auth });
+async function updateSheetData(sheetId, range, values) {
+  const authClient = await auth.getClient();
+  const sheets = google.sheets({ version: 'v4', auth: authClient });
+
   await sheets.spreadsheets.values.update({
     spreadsheetId: sheetId,
-    range: `${sheetName}!B2:B`,
-    valueInputOption: "RAW",
-    requestBody: { values: values.map(v => [v]) }
+    range: range,
+    valueInputOption: 'RAW',
+    requestBody: {
+      values: values,
+    },
   });
 }
 
-module.exports = { getSheetData, updateSheetData };
+module.exports = {
+  getSheetData,
+  updateSheetData,
+};
